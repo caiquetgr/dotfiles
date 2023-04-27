31f9e4a38aa0fdfa2d03e80fee69e5d1d6155891
Caique Borges <caiquetgr@gmail.com>
2023-04-26 23:23:45 -0300

    adding keymappings in lvim, mirroring in ideavim

--- lvim/lua/user/keymaps.lua
+++ lvim/lua/user/keymaps.lua
@@ -1,12 +1,38 @@
+lvim.leader = "space"
+
 lvim.keys.normal_mode["L"] = ":BufferLineCycleNext<CR>"
 lvim.keys.normal_mode["H"] = ":BufferLineCyclePrev<CR>"
 lvim.keys.normal_mode["J"] = ":BufferLinePick<CR>"
+lvim.keys.normal_mode["gi"] = "<cmd>lua vim.lsp.buf.implementation()<cr>"
+lvim.keys.normal_mode["<leader>le"] = {
+    function()
+      local config = lvim.lsp.diagnostics.float
+      config.scope = "line"
+      vim.diagnostic.open_float({ border = "rounded" }, config)
+    end
+}
 
-lvim.builtin.which_key.mappings["f"] = {}
--- -- make ff use =  ["f"] = {
+-- lvim.builtin.which_key.mappings["f"] = {
+--     ["f"] = {
 --         function()
 --           require("lvim.core.telescope.custom-finders").find_project_files { previewer = false }
 --         end,
 --         "Find File",
---       }
-lvim.keys.normal_mode["<leader>ff"] = ":lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>"
+--     }
+-- }
+
+lvim.builtin.which_key.mappings["lE"] = { "<cmd>Telescope quickfix<cr>", "Telescope Quickfix" }
+lvim.builtin.which_key.mappings["le"] = {}
+lvim.builtin.which_key.mappings["le"] = {
+    function()
+      local config = lvim.lsp.diagnostics.float
+      config.scope = "line"
+      vim.diagnostic.open_float({ border = "rounded" }, config)
+    end,
+    "Show line diagnostics"
+}
+
+lvim.builtin.terminal.open_mapping = "<c-t>"
+
+lvim.keys.normal_mode["<leader>ff"] =
+":lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>"
